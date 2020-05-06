06b41c5aedce93fd73dae6c9c978c8994274568d
Kris Gholson <kris@gholson.org>
2020-05-06 09:36:43 -0400

    use the layer from the lambda function

--- handler.js
+++ handler.js
@@ -1,18 +1,24 @@
 'use strict';
+const {exec} = require('child_process')
+const util = require('util')
+const execP = util.promisify(exec)
 
 module.exports.ais2geoson = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+    console.log('execute native command')
+    const result = await execP("echo '2017-03-01T13:41:14;!BSVDM,1,1,,B,13nW`F001V0pk1jV0r5s@8rHP<0i,0*13' | ais2geojson")
+
+    console.log(result);
+    return {
+        statusCode: 200,
+        body: JSON.stringify(
+            {
+                message: 'Go Serverless v1.0! Your function executed successfully!',
+                input: event,
+            },
+            null,
+            2
+        )
+    }
+
+}
