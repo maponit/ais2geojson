2d43f9158c102de0863d2ab915065b572585f49c
Kris Gholson <kris@gholson.org>
2020-05-07 07:54:18 -0400

    make sure that this is an object

--- handler.js
+++ handler.js
@@ -6,9 +6,9 @@ const execP = util.promisify(exec)
 module.exports.ais2geojson = async event => {
 
     const result = await execP(`echo '${event.message}' | ais2geojson`)
+    const geojson = result.stdout
+    console.log(geojson.type)
 
-    console.log(result)
-
-    return result.stdout
+    return geojson
 
 }
