e35915f005a29314a32fa5ef5e1172ce287ce907
Kris Gholson <kris@gholson.org>
2020-05-07 08:01:09 -0400

    shorten it up

--- handler.js
+++ handler.js
@@ -4,11 +4,6 @@ const util = require('util')
 const execP = util.promisify(exec)
 
 module.exports.ais2geojson = async event => {
-
     const result = await execP(`echo '${event.message}' | ais2geojson`)
-    const geojson = JSON.parse(result.stdout)
-    console.log(geojson.type)
-
-    return geojson
-
+    return JSON.parse(result.stdout)
 }
