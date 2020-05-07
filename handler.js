a4c32c53a55931a306ac79a7c48b7dec93ab25b6
Kris Gholson <kris@gholson.org>
2020-05-06 20:26:17 -0400

    commit to build

--- handler.js
+++ handler.js
@@ -3,9 +3,9 @@ const {exec} = require('child_process')
 const util = require('util')
 const execP = util.promisify(exec)
 
-module.exports.ais2geoson = async event => {
+module.exports.ais2geojson = async event => {
 
-    const result = await execP("echo '2017-03-01T13:41:14;!BSVDM,1,1,,B,13nW`F001V0pk1jV0r5s@8rHP<0i,0*13' | /opt/ais2geojson")
+    const result = await execP("echo '2017-03-01T13:41:14;!BSVDM,1,1,,B,13nW`F001V0pk1jV0r5s@8rHP<0i,0*13' | ais2geojson")
 
     console.log(result);
 
